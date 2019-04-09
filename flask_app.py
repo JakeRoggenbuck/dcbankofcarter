8d4ceb142d62876de7c16d989517ebbd03cad82a
IamBillGate <logannhaug@gmail.com>
2019-04-09 09:30:58 -0700

    Successfully created a kind of front end

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def main(): return render_template('login.html')
+
+@app.route('/createanaccount',methods=["GET","POST"])
+def createanaccount():
+	if request.method=="GET":
+		return render_template('createaccount.html')
+	if request.method=="POST":
+		return render_template('accountcreated.html')
 @app.route('/transaction',methods=['POST'])
 def transaction():
 	sender = request.form['sender']
