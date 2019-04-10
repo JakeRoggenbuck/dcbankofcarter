0e8a3d3a926eda143e8c6b992ba16f27d3714451
IamBillGate <logannhaug@gmail.com>
2019-04-10 11:58:31 -0700

    Account creation now works, user id generation also added

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+from random import randint
 
 app = Flask(__name__)
 
@@ -13,9 +14,14 @@ def createanaccount():
 	elif request.method=="POST":
 		conn = sqlite3.connect("userinfo.db")
 		c = conn.cursor()
-		sql = "INSERT into users (user_id, email, password, securityquestion, securityquestionanswer)"
-		sql += "values (79817, " + str(request.form['email']) + ", " + str(request.form['password']) + ", " + str(request.form['SQ']) + ", " + str(request.form['SA']) + ")"
+		sql = "SELECT user_id from users" 
 		c.execute(sql)
+		for x in c.fetchall():
+			id = randint(1,10000)
+			if id != x: break
+		sql = "INSERT into users (user_id, email, password, securityquestion, securityquestionanswer)values ("+ str(id) + ", '" + str(request.form['email']) + "', '" + str(request.form['password']) + "', '" + str(request.form['SQ']) + "', '" + str(request.form['SA']) + "')"		
+		c.execute(sql)
+		conn.commit()
 		conn.close()
 		return render_template('accountcreated.html')
 		
